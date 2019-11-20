6ceee301649712f55be6f1c6072a071535c58118
xu jing <517006882@qq.com>
2019-11-20 13:08:29 +0800

    初始化

---
+++ seat_api/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+// 创建web服务器
+var app = express();
+app.listen(3000, () => {
+  console.log('running--------')
+});
+// 配置跨域
+app.use(cors({
+  'origin': ['http://127.0.0.1:3000', 'http://localhost:3000'],
+  'credentials': true,
+}));
+// 配置session
+app.use(session({
+  secret: 'secret', 
+  resave: false, // 每次请求是否都更新session
+  saveUninitialized: true, //初始化时是否保存数据
+  cookie: {
+    maxAge: 1000 * 60 * 60* 24, // 依靠cookie保存24小时
+  },
+}));
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
---
+++ seat_api/db/pool.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+// 创建连接池对象
+var pool = mysql.createPool({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'root',
+  password: '',
+  database: 'db_seat',
+  connectionLimit: 20,
+  // 执行多条查询语句
+  multipleStatements: true
+});
+// 导出连接池对象
+module.exports = pool;

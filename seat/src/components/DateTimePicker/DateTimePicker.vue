2f68fbfd7002d0cac97117acb766cbbb0ea9afe9
xu jing <517006882@qq.com>
2019-12-19 19:07:35 +0800

    dt_picker_2

--- seat/src/components/DateTimePicker/DateTimePicker.vue
+++ seat/src/components/DateTimePicker/DateTimePicker.vue
@@ -1,13 +1,37 @@
 <template>
   <div id="time-picker">
-    <div class="dp_container">
+    <div class="dp-container">
       <div class="box">
         <div class="choosedate">
           <span class="uni-input mui-icon-extra mui-icon-extra-calendar"> 2019-12-17 </span>
         </div>
         <!-- 日期弹窗 -->
-        <div class="dp_header" style="display:none">
-          
+        <div class="dp-picker-container">
+          <div class="dp-header">
+            <button class="btn-cancel">取消</button>
+            <button class="btn-ok">确定</button>
+          </div>
+          <div class="dp-title" style="display:none">
+            <h5 data-id="title-y">年</h5>
+            <h5 data-id="title-m">月</h5>
+            <h5 data-id="title-d">日</h5>
+            <h5 data-id="title-h">时</h5>
+            <h5 data-id="title-i">分</h5>
+          </div>
+          <div class="dp-body">
+            <div id="picker-y" class="picker">
+              <div class="picker-inner">
+                <div class="picker-rule picker-rule-ft"></div>
+                <ul class="picker-list">
+                  <!-- <li class="highlight">{{new Date()|dateTimeFilter('dateOnly')}}</li>
+                  <li class="visible">{{new Date()|dateTimeFilter('dateOnly')}}</li>
+                  <li class="visible">{{new Date()|dateTimeFilter('dateOnly')}}</li> -->
+                  <li v-for="(item,i) in days" :class="{'highlight':i==0}" :key="i">{{item}}</li>
+                </ul>
+                <div class="picker-rule picker-rule-bg"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="box" style="flex-direction: column;">
@@ -23,7 +47,7 @@
           <div>-----</div>
           <div id="end-time" class="uni-input">18:00</div>
           <div>-----</div>
-          <div id="show-time" class="font_orange">6h</div>
+          <div id="show-time" class="font-orange">6h</div>
         </div>
       </div>
     </div>
@@ -32,7 +56,24 @@
 
 <script>
 export default {
-  props: {}
+  data(){
+    return {
+      shopOpenTime:null,
+      shopCloseTime:null,
+      starTimeMinute:null,
+      starTimeHour:null,
+      endTimeMinute:null,
+      endTimeHour:null,
+      date:null,
+      hours:[],
+      minutes:[],
+      days:[1,2,3]
+    }
+  },
+  props: {},
+  methods:{
+
+  }
 };
 </script>
 
@@ -40,43 +81,104 @@ export default {
 #time-picker  
   width  100% 
 
-  .dp_container  
-    background-color  #efeff4 
+  .dp-container  
+   background-color  #efeff4 
 
-    .box  
-      background-color  #fff 
-      margin  10px 0 
-      border-radius  5Px 
+   .box  
+    background-color  #fff 
+    margin  10px 0 
+    border-radius  5Px 
 
-      // .uni-input
-      //   flex  1
-      //   text-align center
-      //   height  40px
-      //   line-height  40px 
-      //   font-size    24px 
-      //   font-weight  500
+    // .uni-input
+    //  flex  1
+    //  text-align center
+    //  height  40px
+    //  line-height  40px 
+    //  font-size   24px 
+    //  font-weight  500
 
-      .choosedate  
-        height  60px 
-        display  flex 
-        justify-content  center 
-        align-items  center 
-        font-size   24px
-        text-align  center
-        
-      .choosetime  
-        height  40px 
-        display  flex 
-        justify-content  space-around 
-        align-items  center 
+    .choosedate  
+      height  60px 
+      display  flex 
+      justify-content  center 
+      align-items  center 
+      font-size  24px
+      text-align  center
+      
+    .choosetime  
+      height  40px 
+      display  flex 
+      justify-content  space-around 
+      align-items  center 
 
-        >div  
-          flex 1 
+      >div  
+       flex 1 
+       text-align center
+       font-family  '微软雅黑' 
+       font-size  16px 
+       height  40px 
+       line-height  40px 
+
+    .dp-picker-container
+      position fixed 
+      z-index 10000
+      bottom  0
+      width 100%
+      margin 0 -15px
+      border-top  solid 1px #ccc
+      background-color  #eee
+      box-shadow  0 -5px 7px 0 rgba(0,0,0,.1)
+      font-size 18px
+
+      .dp-header
+        width 100%
+        button 
+          background transparent
+          border none
+          padding 15px
+          font-size 20px
+          &:last-child
+            float right
+          &.btn-cancel
+            color gray 
+          &.btn-ok
+            color blue
+
+      .dp-body
+        display flex
+        justify-content center 
+        align-items center
+        position  relative
+        width 100%
+        height  200px
+        .picker
+          flex  1
           text-align center
-          font-family  '微软雅黑' 
-          font-size  16px 
-          height  40px 
-          line-height  40px 
+          .picker-rule-ft  
+            z-index  2 
+            border-top  solid 1px rgba(0,0,0,.1) 
+            border-bottom  solid 1px rgba(0,0,0,.1) 
+          
+          .picker-rule-bg  
+            z-index  0 
+          
+          .picker-list, .picker-rule  
+            box-sizing  border-box 
+            padding  0 
+            margin  -20px 0 0 
+            width  100% 
+            height  36px 
+            line-height  36px 
+            position  absolute 
+            left  0 
+            top  50% 
+          
+          .picker-list li
+            &.highlight
+              color  #222
+            &.highlight,
+            &.visible
+              visibility  visible
 
 </style>
 
--- seat/src/views/SelectSeat/SelectSeat.vue
+++ seat/src/views/SelectSeat/SelectSeat.vue
@@ -4,15 +4,15 @@
     <time-picker></time-picker>
     <div class="seat-example ignore">
       <div class="example example-single">
-        <img src="../../assets/img/seat/seat-single.png" alt="">
+        <img src="../../assets/img/seat/seat-single.png" alt />
         <span>可选</span>
       </div>
       <div class="example example-sold">
-        <img src="../../assets/img/seat/seat-sold.png" alt="">
+        <img src="../../assets/img/seat/seat-sold.png" alt />
         <span>已售</span>
       </div>
       <div class="example example-double">
-        <img src="../../assets/img/seat/seat-double.png" alt="">
+        <img src="../../assets/img/seat/seat-double.png" alt />
         <span>双人座</span>
       </div>
     </div>
@@ -30,118 +30,154 @@
 </template>
 
 <script>
-import {Toast,Indicator,MessageBox} from 'mint-ui'
-import DateTimePicker from '../../components/DateTimePicker/DateTimePicker'
+import { Toast, Indicator, MessageBox } from "mint-ui";
+import DateTimePicker from "../../components/DateTimePicker/DateTimePicker";
 export default {
-  data(){
+  data() {
     return {
       // 选择的座位
-      selectedSeatInfo:{},
+      selectedSeatInfo: {},
       // 座位表
-      seatJson:[],
-    }
+      seatJson: []
+    };
   },
-  components:{
-    "time-picker":DateTimePicker
+  components: {
+    "time-picker": DateTimePicker
   },
-  created(){
-    this.axios.get("seat.json").then(res=>{
-      if(res.data.code==200){//请求成功
-        console.log(res.data.data)
-        this.seatJson=res.data.data[1]
-      }else{
-        console.log("请求的数据不见了，去看一下你的json文件")
+  created() {
+    this.axios.get("seat.json").then(res => {
+      if (res.data.code == 200) {
+        //请求成功
+        console.log(res.data.data);
+        this.seatJson = res.data.data[1];
+      } else {
+        console.log("请求的数据不见了，去看一下你的json文件");
       }
-    })
+    });
   },
-  methods:{
+  methods: {
     // 加载座位信息
     // 选择座位
     // 取消选座
     // 确认选座
-    confirmSelect(){
-      console.log('确认选座')
+    confirmSelect() {
+      console.log("确认选座");
     }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-#select-seat
-  width 100%
-  min-height 100vh
-  padding 60px 15px 0
-  
-  .seat-example
-    // margin  10px 0
-    display flex
-    justify-content center
-    align-items center
-    padding 20px
-    background-color #fff
-    .example
-      flex  1
-      text-align center
-      display flex
-      justify-content center
-      align-items center
-      font-size 18px
-      color #888
-      img 
-        height 30px
-      span 
-        display inline-block
-        padding-left  10px
+#select-seat {
+  width: 100%;
+  min-height: 100vh;
+  padding: 60px 15px 0;
 
-  .seat-outer-container  
-    position relative  
-    overflow hidden
-    width    100%
-    height   calc(100vh - 100px)
-    .seat-inner-container
-      position absolute
-      width   100%
+  .seat-example {
+    // margin  10px 0
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    background-color: #fff;
+
+    .example {
+      flex: 1;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 18px;
+      color: #888;
+
+      img {
+        height: 30px;
+      }
+
+      span {
+        display: inline-block;
+        padding-left: 10px;
+      }
+    }
+  }
+
+  .seat-outer-container {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    height: calc(100vh - 100px);
+
+    .seat-inner-container {
+      position: absolute;
+      width: 100%;
+
       // height  100%
-      .set-position
-        position relative
-        overflow hidden
-        width   100%
-        min-height  1000px
+      .set-position {
+        position: relative;
+        overflow: hidden;
+        width: 100%;
+        min-height: 1000px;
         // height  100%
-        margin  0 auto 
-        .seat
-          position absolute
-          background-repeat  no-repeat
-          background-size    100% 100%
-        .seat-single
-          width   30px
-          height  30px
-        .seat-double
-          width   60px
-          height  30px
-        .seat-bg1
-          background-image url('../../assets/img/seat/seat-single.png')
-        .seat-bg2
-          background-image url('../../assets/img/seat/seat-sold2.png')
-        .seat-bg3
-          background-image url('../../assets/img/seat/single-seat-choose.png')
-        .seat-bg4
-          background-image url('../../assets/img/seat/seat-double.png')
-        .seat-bg5
-          background-image url('../../assets/img/seat/double-seat-sold.png')
-        .seat-bg6
-          background-image url('../../assets/img/seat/double-seat-choose.png')
-  .btn-container   
-    width    100% 
-    position fixed
-    z-index  1000
-    bottom 20px   
-    padding 15px 30px
-    button.submit
-      width 100%
-      height  60px
-      font-size 20px
-      font-weight 500
-      opacity 0.8
+        margin: 0 auto;
+
+        .seat {
+          position: absolute;
+          background-repeat: no-repeat;
+          background-size: 100% 100%;
+        }
+
+        .seat-single {
+          width: 30px;
+          height: 30px;
+        }
+
+        .seat-double {
+          width: 60px;
+          height: 30px;
+        }
+
+        .seat-bg1 {
+          background-image: url('../../assets/img/seat/seat-single.png');
+        }
+
+        .seat-bg2 {
+          background-image: url('../../assets/img/seat/seat-sold2.png');
+        }
+
+        .seat-bg3 {
+          background-image: url('../../assets/img/seat/single-seat-choose.png');
+        }
+
+        .seat-bg4 {
+          background-image: url('../../assets/img/seat/seat-double.png');
+        }
+
+        .seat-bg5 {
+          background-image: url('../../assets/img/seat/double-seat-sold.png');
+        }
+
+        .seat-bg6 {
+          background-image: url('../../assets/img/seat/double-seat-choose.png');
+        }
+      }
+    }
+  }
+
+  .btn-container {
+    width: 100%;
+    position: fixed;
+    z-index: 1000;
+    bottom: 20px;
+    padding: 15px 30px;
+
+    button.submit {
+      width: 100%;
+      height: 60px;
+      font-size: 20px;
+      font-weight: 500;
+      opacity: 0.8;
+    }
+  }
+}
 </style>
 

a87977b2d5f2dc16ced96d9f272f93189a5255ca
xujing <517006882@qq.com>
2020-01-12 21:04:43 +0800

    webpack优化

--- seat/vue.config.js
+++ seat/vue.config.js
@@ -1,8 +1,8 @@
-const webpack = require('webpack')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   //部署应用包时的基本 URL
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist/': '/',
   //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
   outputDir: 'dist',
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
@@ -16,12 +16,53 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       // 为生产环境修改配置...
       config.mode = 'production'
+      config.plugins.push(
+        // 生产环境自动删除console
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            compress: {
+              warnings: false,
+              drop_debugger: true,
+              drop_console: true,
+            },
+          },
+          sourceMap: false,
+          parallel: true,
+        }),
+        // 利用compression-webpack-plugin插件可以帮助进行gzip压缩
+        new CompressionWebpackPlugin({
+          filename: info => {
+            return `${info.path}.gz${info.query}`
+          },
+          algorithm: 'gzip', 
+          threshold: 10240,
+          test: new RegExp(
+            '\\.(' +
+            ['js'].join('|') +
+            ')$'
+          ),
+          minRatio: 0.8,
+          deleteOriginalAssets: false
+         })
+      );
     } else {
       // 为开发环境修改配置...
       config.mode = 'development'
     }
   },  
   chainWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production'){
+      // 删除预加载
+      config.plugins.delete('preload');
+      config.plugins.delete('prefetch');
+      // 压缩代码
+      config.optimization.minimize(true);
+      // 分割代码
+      config.optimization.splitChunks({
+          chunks: 'all'
+      })
+    }
+    // 将vue标签内样式px转换vw
     config.module
       .rule('vue')
       .test(/\.vue$/)
@@ -51,6 +92,13 @@ module.exports = {
         //   '^/': ''
         // }
       }
+    },
+    before(app, server) { 
+      app.get(/.*.(js)$/, (req, res, next) => { 
+        req.url = req.url + '.gz';
+        res.set('Content-Encoding', 'gzip');
+        next();
+      })
     }
   }
 }

d47cb974d13a4d793282d23e41736c5b471cd973
xu jing <517006882@qq.com>
2020-01-13 12:45:10 +0800

    修改

--- seat/vue.config.js
+++ seat/vue.config.js
@@ -3,7 +3,7 @@ const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   //部署应用包时的基本 URL
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? 'http://182.92.118.167:80/dist/' : '/',
   //当运行 vue-cli-service build 时生成的生产环境构建文件的目录
   outputDir: 'dist',
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
